703810864be09f795e586c32b134af984504d185
Mishutko <mishutkoff@gmail.com>
2017-03-04 18:41:52 +0200

    task3solved

--- test.php
+++ test.php
@@ -16,12 +16,12 @@ function checkArg($arg, $type)
 }
 
 //Проверяем тип данных в массиве
-function checkArrayInSide($arg, $type)
+function checkArrayInSide($arg, $type, $type2 = null)
 {
     static $i = 0;
     foreach ($arg as $val)
     {
-        if (gettype($val) == $type)
+        if (gettype($val) == $type || gettype($val) == $type2)
         {
             $i++;
         }
@@ -84,51 +84,51 @@ function checkMathChar($cha)
 // Результат: 1 – 2 – 3 – 4 = -8
 
 
-$myArr = [1, 2, 3, 4];
-
-function homeTask2($arg, $cha)
-{
-    if (checkArg($arg, 'array') && checkMathChar($cha))
-    {
-        if (checkArrayInSide($arg, 'integer') == count($arg))
-        {
-
-            $x = 0;
-            foreach ($arg as $ar => $val)
-            {
-                if ($ar == 0)
-                {
-                    $x = $val;
-                    continue;
-                }
-                else
-                {
-                    switch ($cha)
-                    {
-                        case '-':
-                            $x = $x - $val;
-                            break;
-
-                        case '+':
-                            $x = $x + $val;
-                            break;
-                        case '/':
-                            $x = $x / $val;
-                            break;
-                        case '*':
-                            $x = $x * $val;
-                            break;
-                        default:
-                            die ('Нет такого врефмитического действия, введите правильное (-, +, /, *)');
-                    }
-                }
-            }
-            echo join($cha, $arg) . '=' . $x;
-        }
-    }
-}
-
-homeTask2($myArr, '-');
+//$myArr = [1, 2, 3, 4];
+//
+//function homeTask2($arg, $cha)
+//{
+//    if (checkArg($arg, 'array') && checkMathChar($cha))
+//    {
+//        if (checkArrayInSide($arg, 'integer') == count($arg))
+//        {
+//
+//            $x = 0;
+//            foreach ($arg as $ar => $val)
+//            {
+//                if ($ar == 0)
+//                {
+//                    $x = $val;
+//                    continue;
+//                }
+//                else
+//                {
+//                    switch ($cha)
+//                    {
+//                        case '-':
+//                            $x = $x - $val;
+//                            break;
+//
+//                        case '+':
+//                            $x = $x + $val;
+//                            break;
+//                        case '/':
+//                            $x = $x / $val;
+//                            break;
+//                        case '*':
+//                            $x = $x * $val;
+//                            break;
+//                        default:
+//                            die ('Нет такого врефмитического действия, введите правильное (-, +, /, *)');
+//                    }
+//                }
+//            }
+//            echo join($cha, $arg) . '=' . $x;
+//        }
+//    }
+//}
+//
+//homeTask2($myArr, '-');
 
 
 //______________________________________________________________________________________________________________
@@ -142,3 +142,51 @@ homeTask2($myArr, '-');
 // Например: имя функции someFunction(‘+’, 1, 2, 3, 5.2);
 // Результат: 1 + 2 + 3 + 5.2 = 11.2
 //
+
+function homeTask3()
+{
+    if (checkArg(func_get_args(), "array"))
+    {
+        $myArr = func_get_args();
+        if (checkMathChar(func_get_arg(0)))
+        {
+            unset($myArr[0]);
+            if (checkArrayInSide($myArr, 'integer', 'double'))
+            {
+                $x = 0;
+                foreach ($myArr as $ar => $val)
+                {
+                    if ($ar == 0)
+                    {
+                        $x = $val;
+                        continue;
+                    }
+                    else
+                    {
+                        switch (func_get_arg(0))
+                        {
+                            case '-':
+                                $x = $x - $val;
+                                break;
+
+                            case '+':
+                                $x = $x + $val;
+                                break;
+                            case '/':
+                                $x = $x / $val;
+                                break;
+                            case '*':
+                                $x = $x * $val;
+                                break;
+                            default:
+                                die ('Нет такого врефмитического действия, введите правильное (-, +, /, *)');
+                        }
+                    }
+                }
+                echo join(func_get_arg(0), $myArr) . '=' . $x;
+            }
+        }
+    }
+}
+homeTask3('+', 1, 2, 3, 5.2);
+
